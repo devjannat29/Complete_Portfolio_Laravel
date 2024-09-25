246b4a12f13656bddb1330a9838bf3781a6f4952
Nur Jannat <nurjannat200029@gmail.com>
2024-09-25 17:43:47 +0600

    Personal Portfolio

---
+++ resources/views/frontend/pages/widgets/service.blade.php
@@ -0,0 +1,25 @@
+<section class="service-area section-padding-top" id="about-page">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 text-center">
+          <div class="section-title">
+              <h3 class="title">My Service</h3>
+          </div>
+      </div>
+    </div>
+    <div class="row">
+        @foreach ($services as $service)
+
+        <div class="col-lg-4 {{$loop->index > 2 ? 'mt-4': ''}}">
+            <div class="single-service">
+                <h3 class="title wow fadeInRight" data-wow-delay="0.3s">{{$service->name}}</h3>
+                <div class="desc wow fadeInRight" data-wow-delay="0.4s">
+                    <p>{{$service->description}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+  </div>
+</section>
